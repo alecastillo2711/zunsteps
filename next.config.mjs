9d2239be9700b59a1f826bf7cf0ff9c93e80597b
Carlos Jesus Borges Milian <carlosjbm11780@gmail.com>
2025-11-27 13:56:56 -0500

    chore: agregar netlify.toml para el despliegue

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/zunsteps",
-  assetPrefix: "/zunsteps",
   trailingSlash: true,
   images: { unoptimized: true },
 };
